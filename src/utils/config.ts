a3487a56b3f598ed7c60f5a692915edf1156f761
McSam <sam@raccoons.dev>
2022-11-07 15:14:25 +0000

    get price account

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,12 @@
+import { find } from 'lodash';
 import lendConfig from '../constants/lending_info.json';
 
 export const getLendingMarketAccount = () => {
   return lendConfig.lending.lending_market_account;
+};
+
+export const getPriceFeedsForReserve = (name) => {
+  return find(lendConfig.pyth.price_feeds, (priceFeed) => {
+    return priceFeed.name === name;
+  });
 };
